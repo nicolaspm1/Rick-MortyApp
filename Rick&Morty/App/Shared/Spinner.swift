7371c947ce6b122f9aeb7df19f22a29bb7ee5c64
Pablo Manzur <p.manzur@hotmail.es>
2022-08-30 23:14:15 -0300

    Add Spinner to reuse

---
+++ Rick&Morty/App/Shared/Spinner.swift
@@ -0,0 +1,40 @@
+//
+//  Spinner.swift
+//  Rick&Morty
+//
+//  Created by Pablo Manzur on 30/08/2022.
+//
+
+import UIKit
+
+
+var spinnerView: UIView?
+
+extension UIViewController {
+    
+    func showSpinner() {
+        
+        spinnerView = UIView.init(frame: view.bounds)
+        spinnerView?.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        
+        let spinner = UIActivityIndicatorView.init(style: .large)
+        spinner.color = .red
+        spinner.center = spinnerView!.center
+        spinner.startAnimating()
+        
+        DispatchQueue.main.async {
+            spinnerView?.addSubview(spinner)
+            self.view.addSubview(spinnerView!)
+        }
+        
+    }
+    
+    func removeSpinner() {
+        
+        DispatchQueue.main.async {
+            spinnerView?.removeFromSuperview()
+            spinnerView = nil
+        }
+    }
+    
+}
