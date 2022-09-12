7736584cc3db5ef34459dff8ca88572237b2ba9a
Pablo Manzur <p.manzur@hotmail.es>
2022-09-12 18:48:41 -0300

    HomeService functions defined

--- Rick&Morty/App/Modules/HomeDataService.swift
+++ Rick&Morty/App/Modules/HomeDataService.swift
@@ -9,8 +9,84 @@ import Foundation
 
 protocol HomeDataRepositoryProtocol {
     
+    func fetchCharacters(onComplete: @escaping ([Character]) -> Void, onError: @escaping (String) -> Void)
+    func fetchEpisodes(onComplete: @escaping ([Episode]) -> Void,  onError: @escaping (String) -> Void)
+    func fetchLocations(onComplete: @escaping ([Locations]) -> Void, onError: @escaping (String) -> Void)
+    
 }
 
 class HomeDataService: HomeDataRepositoryProtocol {
+    
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        return decoder
+    }()
+    
+    func fetchCharacters(onComplete: @escaping ([Character]) -> Void, onError: @escaping (String) -> Void) {
+        
+        APIManager.shared.get(url: KUrl.characters) { response in
+            switch response {
+                    
+                case .success(let data):
+                    do {
+                        if let data = data {
+                            self.decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            let characterResponse = try self.decoder.decode(CharactersResponse.self, from: data)
+                            onComplete(characterResponse.results)
+                        }
+                        
+                    } catch {
+                        onError(error.localizedDescription)
+                    }
+                case .failure(let fail):
+                    onError(fail.localizedDescription)
+            }
+        }
+    }
+    
+    func fetchEpisodes(onComplete: @escaping ([Episode]) -> Void, onError: @escaping (String) -> Void) {
+        
+        APIManager.shared.get(url: KUrl.episodes) { response in
+            switch response {
+                    
+                case .success(let data):
+                    do {
+                        if let data = data {
+                            self.decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            let episodeResponse = try self.decoder.decode(EpisodesResponse.self, from: data)
+                            onComplete(episodeResponse.results)
+                        }
+                        
+                    } catch {
+                        onError(error.localizedDescription)
+                    }
+                case .failure(let fail):
+                    onError(fail.localizedDescription)
+            }
+        }
+    }
+    
+    func fetchLocations(onComplete: @escaping ([Locations]) -> Void, onError: @escaping (String) -> Void) {
+        
+        APIManager.shared.get(url: KUrl.locations) { response in
+            switch response {
+                    
+                case .success(let data):
+                    do {
+                        if let data = data {
+                            self.decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            let locationsResponse = try self.decoder.decode(LocationsResponse.self, from: data)
+                            onComplete(locationsResponse.results)
+                        }
+                        
+                    } catch {
+                        onError(error.localizedDescription)
+                    }
+                case .failure(let fail):
+                    onError(fail.localizedDescription)
+            }
+        }
+    }
+    
     
 }
---
+++ Rick&Morty/App/Modules/HomeViewModel.swift
@@ -0,0 +1,87 @@
+//
+//  HomeViewModel.swift
+//  Rick&Morty
+//
+//  Created by Pablo Manzur on 12/09/2022.
+//
+
+import Foundation
+
+protocol HomeDelegate: AnyObject {
+    func loading()
+    func stop()
+    func reloadData()
+    func show(error: String)
+}
+
+
+class HomeViewModel {
+    
+    private var characters = [Character]()
+    private var episodes = [Episode]()
+    private var locations = [Locations]()
+    
+    weak var delegate: HomeDelegate?
+    var homeService: HomeDataRepositoryProtocol?
+    
+    init(homeService: HomeDataRepositoryProtocol = HomeDataService()) {
+        self.homeService = homeService
+    }
+    
+}
+
+//MARK: - Get characters
+extension HomeViewModel {
+    
+    func getCharacters() {
+        homeService?.fetchCharacters(onComplete: { characters in
+            self.characters = characters
+            self.delegate?.loading()
+            self.delegate?.reloadData()
+        }, onError: { error in
+            self.delegate?.show(error: error)
+        })
+    }
+    
+    func getCharactersCount() -> Int {
+        characters.count
+    }
+    
+    func getCharacterAt(index: Int) -> Character {
+        characters[index]
+    }
+}
+
+//MARK: - get episodes
+extension HomeViewModel{
+    
+    func getEpisodes() {
+        
+    }
+    
+    func getEpisodesCount() -> Int {
+        episodes.count
+    }
+    
+    func getEpisodeAt(index: Int) -> Episode {
+        episodes[index]
+    }
+}
+
+
+
+//MARK: - get locations
+extension HomeViewModel {
+    
+    func getLocations() {
+        
+    }
+    
+    func getLocationsCount() -> Int {
+        locations.count
+    }
+    
+    func getLocationAt(index: Int) -> Locations {
+        locations[index]
+    }
+}
